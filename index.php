a982be80c4047e574de6b5ed36f58bafa74b95b2
Mike Jaren Yap <mjaren.yap@gmail.com>
2017-11-03 00:02:00 +0800

    pushed initial code to master

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>PEB Online - The Official Student Online Publication of Lourdes School Quezon City High School Department</title>
+		<link rel="stylesheet" type="text/css" href="css/theme.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+	</head>
+	<body>
+		<header>
+			<img src="assets/images/peb-logo-white.png" id="main-logo" />
+			<h5>PEB Online - The Official Student Online Publication of Lourdes School Quezon City <br/>High School Department</h5>
+		</header>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>News</li>
+				<li>Opinion</li>
+				<li>Features</li>
+				<li>Sports</li>
+				<li>Literary</li>
+			</ul>
+			<ul class="social-nav">
+				<li><i class="fa fa-facebook"></i></li>
+				<li><i class="fa fa-twitter"></i></li>
+				<li><i class="fa fa-youtube"></i></li>
+			</ul>
+		</nav>
+		<section>
+			<div class="content-wrapper">
+				<h1>Hello World!</h1>
+				<p>
+					This is a paragraph. This is another sentence. This is a paragraph. This is another sentence.This is a paragraph. This is another sentence. This is a paragraph. This is another sentence.This is a paragraph. This is another sentence.This is a paragraph. This is another sentence.This is a paragraph. This is another sentence.
+				</p>
+			</div>
+		</section>
+	</body>
+</html>
